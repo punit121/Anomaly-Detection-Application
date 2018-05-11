920af0afffa3142d37ab7820a65669887875f49b
PUNIT <punit@nsg-static-002.193.71.182.airtel.in>
2018-05-11 15:13:59 +0530

    code added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,37 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	 @Bean
+	    CommandLineRunner init(DomainRepository domainRepository) {
+
+	        return args -> {
+
+	            Domain obj = domainRepository.findOne(7L);
+	            System.out.println(obj);
+
+	            Domain obj2 = domainRepository.findFirstByDomain("mkyong.com");
+	            System.out.println(obj2);
+
+	            int n = domainRepository.updateDomain("mkyong.com", true);
+	            System.out.println("Number of records updated : " + n);
+
+	        };
+
+	    }
 }
+
+
